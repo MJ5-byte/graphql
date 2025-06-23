b4eca1f69548376b9308815a79aa8ca2eb4cc28c
mohamedahmed0 <mj1136180@gmail.com>
2025-06-23 18:15:15 +0300

    files manged

--- query.js
+++ query.js
@@ -9,24 +9,6 @@ export const USER_QUERY = `
       login
       totalDown
       totalUp
-      groupsByCaptainid {
-        campus
-        captainId
-        captainLogin
-        createdAt
-        eventId
-        id
-        objectId
-        path
-        status
-        updatedAt
-      }
-      TransactionsFiltered1: transactions(where: {type: {_eq: "xp"}, path: { _like: "%bh-module%", _nregex: "^.(piscine-js/|piscine-rust/|piscine-ui/|piscine-ux/)." }}) {
-        amount
-        type
-        path
-        createdAt
-      }
     }
     event_user(where: { eventId: { _in: [72, 20, 250] } }) {
       level
